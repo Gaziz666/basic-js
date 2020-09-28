c9dec50a3e2954ea1963c6c5fb3c5ac1c2b271a5
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-09-28 21:32:25 +0300

    feat: finish simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  value: [],
+  finish: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.value.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.value.push('(  )')
+      return this
+    }
+    this.value.push('( ' + value + ' )')
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (Number.isInteger(position) && position > 0 && position <= this.value.length) {
+      this.value.splice(position - 1, 1)
+      return this
+    }
+    this.value = [];
+    throw new Error;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.value.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.finish = this.value
+    this.value = []
+    return this.finish.join('~~')
   }
 };
 
