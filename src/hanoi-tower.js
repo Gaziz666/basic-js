742bfc8dc3b83762e2be8b33a578563c27aded65
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-09-28 21:29:43 +0300

    feat: finish hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = Math.pow(2, disksNumber) -1;
+  let turnsSpeedPS = 3600 / turnsSpeed
+  let seconds = turns * turnsSpeedPS;
+  let result = {
+    "turns": turns,
+    "seconds": Math.floor(seconds)
+  }
+  return result
 };
