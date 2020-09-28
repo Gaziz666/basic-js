43f6fca73406f51f8ecd6924e5203c4c70d2a498
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-09-28 21:30:46 +0300

    feat: finish transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Object.prototype.toString.call(arr) !== "[object Array]") {
+    console.log("not arr")
+    throw new Error
+  }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      result.push('');
+      result.push('')
+      i += 1;
+    } else if (arr[i] === "--discard-prev") {
+      if (i === 0 || result[i - 1] === '') {
+        result.push('');
+        continue;
+      }
+      result[i - 1] = '';
+      result.push('')
+    } else if (arr[i] === "--double-next") {
+      if (i === arr.length - 1 || arr[i + 1] === '') {
+        continue
+      };
+      result.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev") {
+      if (i === 0 || result[i - 1] === '' || arr[i - 1] === '') {
+        result.push('');
+        continue
+      };
+      result.push(arr[i - 1]);
+    } else {
+      result.push(arr[i])
+    }
+  }
+  return result.filter(item => item !== "")
 };
